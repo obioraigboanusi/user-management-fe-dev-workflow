ff192b286f0b3618d61a22b98f5681a313cc8f14
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2026-09-24 12:05:19 +0100

    Add Prettier and ESLint configuration for code formatting and linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{ts,tsx}'],
+
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier,
     ],
+
+    plugins: {
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+    },
+
     languageOptions: {
       globals: globals.browser,
     },
   },
-])
+]);
